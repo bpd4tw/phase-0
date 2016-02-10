10ebec3f17020011b8cabbd9f42f90f189de9dc1
Brian Donahue <bpd4tw@virginia.edu>
2016-02-10 18:59:46 -0500

    Use absolute links to 4.3 challenges

--- week-4/4.3/variables-methods.rb
+++ week-4/4.3/variables-methods.rb
@@ -1,6 +1,6 @@
-#[Defining a method](define-method/my_solution.rb)
-#[Return a formatted address](address/my_solution.rb)
-#[Defining Math Methods](math/my_solution.rb)
+[Defining a method] https://github.com/bpd4tw/phase-0/blob/master/week-4/4.3/define-method/my_solution.rb
+[Return a formatted address] https://github.com/bpd4tw/phase-0/blob/master/week-4/4.3/address/my_solution.rb
+[Defining Math Methods]https://github.com/bpd4tw/phase-0/blob/master/week-4/4.3/math/my_solution.rb
 
 puts "Hi, what's your first name?"
 first_name=gets.chomp
@@ -15,20 +15,20 @@ fav_number=gets.chomp
 better=fav_number.to_i + 1
 puts "Try this number out: " + better.to_s + ". Now that's a big number!"
 
-##How do you define a local variable?
-###you define a local variable with an "=" sign.
+#How do you define a local variable?
+#you define a local variable with an "=" sign.
 
-##How do you define a method?
-###You define a method using "def" to start, and ending with a paramter "( )" to accept arguments.
+#How do you define a method?
+#You define a method using "def" to start, and ending with a paramter "( )" to accept arguments.
 
-##What is the difference between a local variable and a method?
-###Ruby methods bundle one or more "statements" into a single block of code. Variables are memory locations that store values and are called upon by typing a given variables name. Methods, then, are mainly comprised of different variables that have already been defined. Instead of writing multiple variables to execute code, a developer can utilize methods to bundle them altogether and save time.
+#What is the difference between a local variable and a method?
+#Ruby methods bundle one or more "statements" into a single block of code. Variables are memory locations that store values and are called upon by typing a given variables name. Methods, then, are mainly comprised of different variables that have already been defined. Instead of writing multiple variables to execute code, a developer can utilize methods to bundle them altogether and save time.
 
-##How do you run a ruby program from the command line?
-###To run a ruby program from the command line, type "ruby file_name.rb"
+#How do you run a ruby program from the command line?
+#To run a ruby program from the command line, type "ruby file_name.rb"
 
-##How do you run an RSpec file from the command line?
-###Type "rspec file_name_spec.rb"
+#How do you run an RSpec file from the command line?
+#Type "rspec file_name_spec.rb"
 
-##What was confusing about this material? What made sense?
-###The material is all pretty straight forward. I just need to make sure to pay attention to smaller details like "puts" vs "return" when writing methods.
+#What was confusing about this material? What made sense?
+#The material is all pretty straight forward. I just need to make sure to pay attention to smaller details like "puts" vs "return" when writing methods.
