a286a91b2a679c92da513e9102c3948ebb7f2785
Brian Donahue <bpd4tw@virginia.edu>
2016-03-02 17:29:35 -0500

    Complete release 1 for 7.8 - more to still complete

---
+++ week-7/7.8/group_project_solution.js
@@ -0,0 +1,7 @@
+//Release 1 : User Story - Brian Donahue
+//(Writing from the perspective of a potential user of these javascript functions)
+//As a user, I have two different arrays that need to be molded. The first array has an odd length of indices. The numbers inside the array are 1,2,3,4,5,5,7. The other array has an even number indices. The numbers are 4,4,5,5,6,6,6,7. I simply want to call JavaScript functions on these two arrays to receive specific outputs.
+//The first function I want to use will sum all the values within the odd length array to have a value of 27. Using the sum function on the even array should produce an ouput 43.
+//Next, I want to find the mean of each array. If I call mean and use the odd length array as my argument, the result should return 3.857142857142857. Calling mean on the even length array should return 5.375.
+//Lastly, I'll want to be able to call on a median function.  If I use the median function on the odd length array, it should return 4. Using the median function on the even length array should return 5.5.
+//...So, that's it! Simply put, I want to interact with two different arrays using three functions (sum, mean, median).
