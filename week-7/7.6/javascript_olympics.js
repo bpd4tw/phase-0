89e713010cf62a8e41a3ce7a7178453ff9c94586
Brian Donahue <bpd4tw@virginia.edu>
2016-03-03 20:07:53 -0500

    Complete 7.6 with reflections

--- week-7/7.6/javascript_olympics.js
+++ week-7/7.6/javascript_olympics.js
@@ -64,7 +64,7 @@ function Athlete(name, age, sport, quote){
   this.age = age;
   this.sport = sport;
   this.quote = quote;
-}
+};
 
 
 
@@ -77,4 +77,15 @@ console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps
 
 
 
-// Reflection
+// // Reflection
+// What JavaScript knowledge did you solidify in this challenge?
+//Interacting with JavaScript objects. Also, it was nice to go over "for" conditions again.
+//Plus, we got some more practice with built in functions.
+
+// What are constructor functions?
+//A constructor is a JavaScript function.Contructor functions are another way of creating an object in JavaScript. They are essentially the same as
+//literal objects, but the syntax is different for each. Calling a new instance of a constructor function
+//is similar to making a new object for a class in Ruby.
+
+// How are constructors different from Ruby classes (in your research)?
+// A constructor is a JavaScript function. When used with the "new" keyword,it makes a new object and initializes the new object with a prototype. Hence, constructors and prototypes work in unison. The constructor manufactures new objects and the prototype defines the behaviour of new objects manufactured by the constructor. Prototypes are not classes, though,  and neither are constructor functions bundled with prototypes. They don’t encapsulate their private data, and they don’t have a metaclass object defining their behaviour like Ruby classes do.
