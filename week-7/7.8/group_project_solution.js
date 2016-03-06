3cf6f607a73391c0a54b48fb555f36052ec19a71
Brian Donahue <bpd4tw@virginia.edu>
2016-03-06 15:48:02 -0500

    Complete release 3 coding portion of 7.8 group project

--- week-7/7.8/group_project_solution.js
+++ week-7/7.8/group_project_solution.js
@@ -5,3 +5,74 @@
 //Next, I want to find the mean of each array. If I call mean and use the odd length array as my argument, the result should return 3.857142857142857. Calling mean on the even length array should return 5.375.
 //Lastly, I'll want to be able to call on a median function.  If I use the median function on the odd length array, it should return 4. Using the median function on the even length array should return 5.5.
 //...So, that's it! Simply put, I want to interact with two different arrays using three functions (sum, mean, median).
+
+//Release 2
+// Psudocode:  Both of the data structures need to made with the numbers required from the user.
+
+// One data structure will house all odd numbers, one will hold all even numbers.
+
+// We will need to calculate the data in these functions in three different ways: sum, mean and median
+
+// Input: the input into the function will be one of the arrays
+// Output: the values from the arrays will produce a mathematical answer in the form of sum, mean and median
+
+// Steps: Make the data structures
+
+// Make functions that accept an array as an argument
+
+// Use built in javascript methods to form these calculations
+
+// print out calculations to the user in a fancy style.
+
+//Release 3 : Initial Code
+
+//sum function
+function sum(array){
+  var total = 0;
+  for (var i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  console.log(total);
+};
+
+var oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+var evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+sum(oddLengthArray)
+sum(evenLengthArray)
+
+//mean function
+function mean(array){
+  var total = 0
+  for (var i=0; i < array.length; i++){
+    total += array[i];
+  }
+  var mean = total/array.length;
+  console.log(mean);
+}
+
+var oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+var evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+mean(oddLengthArray)
+mean(evenLengthArray)
+
+//median function
+function median(array) {
+    var sorted = array.sort( function(a,b) {return a - b;} );
+    var middle = Math.floor(sorted.length/2);
+
+    if(array.length % 2){
+      console.log(array[middle]);
+    }
+    else{
+      console.log((array[middle-1] + array[middle]) / 2.0);
+    }
+};
+
+
+var oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
+var evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
+
+median(oddLengthArray)
+median(evenLengthArray)
